cd1717bb8e8174241184696cfed66a02cffc4ec8
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2026-05-12 14:53:25 +0400

    update page and input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="h-screen w-full flex items-center justify-center">
+    <div className="h-screen w-full flex flex-col items-center justify-center">
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
         Discuss
       </h1>
+      <div className="flex flex-row justify-center gap-3 w-2/5">
+        <Link className="w-20 border p-2 rounded-md text-center" href="/login">Login</Link>
+        <Link className="w-20 border p-2 rounded-md text-center" href="/register">Register</Link>
+      </div>
     </div>
   );
 }
