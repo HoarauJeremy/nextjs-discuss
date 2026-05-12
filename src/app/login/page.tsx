4e4c3cbb54ba6a85a590693a9531ba5862b19e9a
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2026-05-12 15:13:02 +0400

    update login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -38,23 +38,25 @@ export default function LoginPage() {
     <div className="mx-auto justify-center flex items-center w-1/2 h-screen">
       <form className="flex flex-col px-2.5 w-2/3 h-1/2" onSubmit={handleSubmit}>
         <input
-          className="border dark:border-white p-2.5 mb-2 rounded-md focus:outline-2 focus:outline-offset-2 focus:outline-white"
+          className="bg-zinc-800 ring ring-zinc-700 p-2.5 mb-2.5 rounded-md focus:outline-2 focus:outline-offset-2 focus:outline-white"
           type="text"
           placeholder="email"
           onChange={(e) => setEmail(e.target.value)}
           />
-        <div className="mb-2 flex">
+        <div className="mb-2.5 flex">
           <input
-            className="w-11/12 border dark:border-white p-2.5 rounded-md focus:outline-2 focus:outline-offset-2 dark:focus:outline-white"
+            className="w-11/12 bg-zinc-800 ring-zinc-700 p-2.5 rounded-md focus:outline-2 focus:outline-offset-2 dark:focus:outline-white"
             type={type}
             placeholder="password"
             onChange={(e) => setPassword(e.target.value)}
-            />
-          <span className="w-1/12 flex justify-around items-center" onClick={handleToggle}>
-            <FaRegEye />
-          </span>
+          />
+          <div className="w-1/12 flex justify-center cursor-pointer">
+            <span className="w-10 bg-zinc-800 flex items-center justify-center rounded-md ring-1 ring-zinc-700 focus:outline-2 focus:outline-offset-2 focus:outline-white" onClick={handleToggle}>
+              <FaRegEye />
+            </span>
+          </div>
         </div>
-        <button className="border dark:border-white p-2.5 mb-2 rounded-md focus:outline-2 focus:outline-offset-2 focus:outline-white dark:hover:bg-white dark:hover:text-zinc-950 transition delay-150 duration-300 ease-in-out" type="submit" disabled={loading}>
+        <button className="bg-zinc-800 ring-zinc-700 p-2.5 mb-2.5 rounded-md focus:outline-2 focus:outline-offset-2 focus:outline-white dark:hover:bg-white dark:hover:text-zinc-950 transition delay-150 duration-300 ease-in-out" type="submit" disabled={loading}>
           {loading ? <PacmanLoader className="mx-auto" color="white" /> : "Se connecter"}
         </button>
       </form>
